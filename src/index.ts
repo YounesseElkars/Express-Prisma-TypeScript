53b971b0972b0372dd3a72d31aabeb30c584d795
Unesdevdev <y.ounesseelkars9@gmail.com>
2024-01-23 18:51:24 +0100

    adding routes to index

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import * as dotenv from 'dotenv';
 import express from 'express';
 import cors from 'cors';
+import { authorRouter } from './author/author.router';
+import { bookRouter } from './book/book.router';
 
 dotenv.config();
 
@@ -11,6 +13,9 @@ const app = express();
 app.use(cors());
 
 app.use(express.json());
+
+app.use('/api/authors', authorRouter);
+app.use('/api/books', bookRouter);
 
 app.listen(PORT, () => {
   console.log(`Listening on PORT ${PORT}`);
