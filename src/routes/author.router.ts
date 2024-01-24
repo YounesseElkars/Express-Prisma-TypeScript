c85536c0b3734dd901008b118106f72bcc9907e1
Unesdevdev <y.ounesseelkars9@gmail.com>
2024-01-24 15:10:24 +0100

    checkExistingAuthor middleware in author router

--- src/routes/author.router.ts
+++ src/routes/author.router.ts
@@ -8,7 +8,7 @@ router.get('/', AuthorController.listAuthors);
 
 // GET : Get One author by ID
 // Params query : id
-router.get('/:id', AuthorController.getAuthor);
+router.get('/:id', AuthorController.checkExistingAuthor, AuthorController.getAuthor);
 
 // POST : Create one author
 // Params body : firstName , lastName
@@ -17,10 +17,10 @@ router.post('/', AuthorController.createAuthor);
 // PUT : update an author
 // Params query : id
 // Params body : firstName , lastName
-router.put('/:id', AuthorController.updateAuthor);
+router.put('/:id', AuthorController.checkExistingAuthor, AuthorController.updateAuthor);
 
 // DELETE : delete an author
 // Params query : id
-router.delete('/:id', AuthorController.deleteAuthor);
+router.delete('/:id', AuthorController.checkExistingAuthor, AuthorController.deleteAuthor);
 
 export default router;
