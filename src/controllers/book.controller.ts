73e5bcf783c342f8239f55d8e459501aed846cb3
Unesdevdev <y.ounesseelkars9@gmail.com>
2024-01-24 14:52:29 +0100

    FEAT : removing error handling from  controller , adding checkauthorexist , checkbookexist as middleware

--- src/controllers/book.controller.ts
+++ src/controllers/book.controller.ts
@@ -1,99 +1,96 @@
 import HttpStatusCode from '../utils/HttpStatusCode';
 import * as BookService from '../services/book.service';
 import * as AuthorService from '../services/author.service';
-import { Request, Response } from 'express';
+import { NextFunction, Request, Response } from 'express';
 import { bookSchema } from '../types/zod';
-import { z } from 'zod';
 
-export const listBooks = async (request: Request, response: Response) => {
+export const listBooks = async (request: Request, response: Response, next: NextFunction) => {
   try {
     const books = await BookService.listBooks();
     return response.status(HttpStatusCode.OK).json(books);
   } catch (error: any) {
-    return response.status(HttpStatusCode.INTERNAL_SERVER_ERROR).json(error.message);
+    next(error);
   }
 };
 
-export const getBook = async (request: Request, response: Response) => {
+export const getBook = async (request: Request, response: Response, next: NextFunction) => {
   try {
     const id = parseInt(request.params.id, 10);
     const book = await BookService.getBook(id);
-    if (book) {
-      return response.status(HttpStatusCode.OK).json(book);
-    }
-    return response.status(HttpStatusCode.NOT_FOUND).json({
-      status: 'Error',
-      message: 'Book Not Found',
-    });
+    return response.status(HttpStatusCode.OK).json(book);
   } catch (error: any) {
-    return response.status(HttpStatusCode.INTERNAL_SERVER_ERROR).json(error.message);
+    next(error);
   }
 };
 
-export const createBook = async (request: Request, response: Response) => {
+export const createBook = async (request: Request, response: Response, next: NextFunction) => {
   try {
     const book = request.body;
     book.datePublished = new Date(book.datePublished);
     const data = bookSchema.parse(book);
-    const existingAuthor = await AuthorService.getAuthor(data.authorId);
-    if (!existingAuthor) {
-      return response.status(HttpStatusCode.NOT_FOUND).json({ success: false, error: 'Author does not exist' });
-    }
     const newBook = await BookService.createBook(data);
     return response.status(HttpStatusCode.CREATED).json(newBook);
   } catch (error: any) {
-    if (error instanceof z.ZodError) {
-      return response
-        .status(HttpStatusCode.BAD_REQUEST)
-        .json({ success: false, errors: error.errors.map((e: any) => e.message) });
-    } else {
-      return response.status(HttpStatusCode.INTERNAL_SERVER_ERROR).json(error.message);
-    }
+    next(error);
   }
 };
 
-export const updateBook = async (request: Request, response: Response) => {
+export const updateBook = async (request: Request, response: Response, next: NextFunction) => {
   try {
     const id = parseInt(request.params.id, 10);
-    // Check if the book exist
-    const existingBook = await BookService.getBook(id);
-    if (!existingBook) {
-      return response.status(HttpStatusCode.NOT_FOUND).json({ success: false, error: 'Book does not exist' });
-    }
     const book = request.body;
     book.datePublished = new Date(book.datePublished);
     const data = bookSchema.parse(book);
-    const existingAuthor = await AuthorService.getAuthor(data.authorId);
-    if (!existingAuthor) {
-      return response.status(HttpStatusCode.NOT_FOUND).json({ success: false, error: 'Author does not exist' });
-    }
     const updateBook = await BookService.updateBook(data, id);
     return response.status(HttpStatusCode.OK).json(updateBook);
   } catch (error: any) {
-    if (error instanceof z.ZodError) {
-      return response
-        .status(HttpStatusCode.BAD_REQUEST)
-        .json({ success: false, errors: error.errors.map((e: any) => e.message) });
-    } else {
-      return response.status(HttpStatusCode.INTERNAL_SERVER_ERROR).json(error.message);
-    }
+    next(error);
   }
 };
 
-export const deleteBook = async (request: Request, response: Response) => {
+export const deleteBook = async (request: Request, response: Response, next: NextFunction) => {
   try {
     const id = parseInt(request.params.id, 10);
-    // Check if the book exist
-    const existingBook = await BookService.getBook(id);
-    if (!existingBook) {
-      return response.status(HttpStatusCode.NOT_FOUND).json({ success: false, error: 'Book does not exist' });
-    }
     await BookService.deleteBook(id);
     return response.status(HttpStatusCode.OK).json({
       success: true,
       message: 'Author has been deleted',
     });
   } catch (error: any) {
-    return response.status(HttpStatusCode.INTERNAL_SERVER_ERROR).json(error.message);
+    next(error);
+  }
+};
+
+export const checkExistingBook = async (request: Request, response: Response, next: NextFunction) => {
+  try {
+    const id = parseInt(request.params.id, 10);
+    const existingBook = await BookService.getBook(id);
+    if (!existingBook) {
+      return response.status(HttpStatusCode.NOT_FOUND).json({
+        status: 'Error',
+        message: 'Book Not Found',
+      });
+    }
+    next();
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const checkExistingBookAuthor = async (request: Request, response: Response, next: NextFunction) => {
+  try {
+    const authorId: number = request.body.authorId;
+
+    const existingAuthor = await AuthorService.getAuthor(authorId);
+
+    if (!existingAuthor) {
+      return response.status(HttpStatusCode.NOT_FOUND).json({
+        status: 'Error',
+        message: 'Author Not Found',
+      });
+    }
+    next();
+  } catch (error) {
+    next(error);
   }
 };
--- src/routes/book.router.ts
+++ src/routes/book.router.ts
@@ -8,18 +8,19 @@ router.get('/', BookController.listBooks);
 
 // GET : Get One book by ID
 // Params query : id
-router.get('/:id', BookController.getBook);
+router.get('/:id', BookController.checkExistingBook, BookController.getBook);
 
 // POST : Create one book
 // Params body : title , authorId , datePublished , isFiction
-router.post('/', BookController.createBook);
+router.post('/', BookController.checkExistingBookAuthor, BookController.createBook);
 
 // PUT : update one book
+// Params query : id
 // Params body : title , authorId , datePublished , isFiction
-router.put('/:id', BookController.updateBook);
+router.put('/:id', BookController.checkExistingBook, BookController.checkExistingBookAuthor, BookController.updateBook);
 
 // DELETE : delete a book
 // Params query : id
-router.delete('/:id', BookController.deleteBook);
+router.delete('/:id', BookController.checkExistingBook, BookController.deleteBook);
 
 export default router;
