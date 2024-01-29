2e581d414e160b34e17a71aaf7a5daf1ecbe0385
Unesdevdev <y.ounesseelkars9@gmail.com>
2024-01-30 00:00:11 +0100

    zod schemas

--- src/types/zod.ts
+++ src/types/zod.ts
@@ -1,5 +1,7 @@
 import { z } from 'zod';
 
+// _____________  Author Schema  _____________
+
 export const authorSchema = z.object({
   firstName: z.string().min(1, { message: 'Your first name must be at least 1 characters long' }).max(30, {
     message: 'your first name cannot be longer than 30 characters',
@@ -9,6 +11,8 @@ export const authorSchema = z.object({
   }),
 });
 
+// _____________  Book Schema  _____________
+
 export const bookSchema = z.object({
   title: z.string().min(1, { message: 'title must be at least 1 characters long' }).max(250, {
     message: 'title cannot be longer than 250 characters',
@@ -17,3 +21,31 @@ export const bookSchema = z.object({
   datePublished: z.date(),
   isFiction: z.boolean(),
 });
+
+// _____________  User Schema  Login  _____________
+
+const userBaseSchema = {
+  username: z.string().min(1, { message: 'username must be at least 1 characters long' }).max(50, {
+    message: 'username cannot be longer than 50 characters',
+  }),
+  password: z.string().min(1, { message: 'password must be at least 1 characters long' }).max(50, {
+    message: 'password cannot be longer than 50 characters',
+  }),
+};
+
+export const userSchema = z.object(userBaseSchema);
+
+// _____________  User Update Schema   _____________
+
+export const userUpdateSchema = z.object({
+  ...userBaseSchema,
+  fullName: z.string().min(1, { message: 'fullName must be at least 1 characters long' }).max(50, {
+    message: 'fullName cannot be longer than 50 characters',
+  }),
+  email: z.string().email({ message: 'Invalid email address' }),
+});
+
+// _____________  Export Types   _____________
+
+export type TUserSchema = z.infer<typeof userSchema>;
+export type TuserUpdateSchema = z.infer<typeof userUpdateSchema>;
