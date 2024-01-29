f13837b98430eabd322b7fcbdaf441f80a8eb382
Unesdevdev <y.ounesseelkars9@gmail.com>
2024-01-29 23:59:09 +0100

    feat : book router

--- src/routes/book.router.ts
+++ src/routes/book.router.ts
@@ -3,17 +3,21 @@ import * as BookController from '../controllers/book.controller';
 
 const router = express.Router();
 
+// Acess : Public
 // GET: List all the books
 router.get('/', BookController.listBooks);
 
+// Acess : Public
 // GET : Get One book by ID
 // Params query : id
 router.get('/:id', BookController.checkExistingBook, BookController.getBook);
 
+// Acess : Private
 // POST : Create one book
 // Params body : title , authorId , datePublished , isFiction
 router.post('/', BookController.validateBookData, BookController.checkExistingBookAuthor, BookController.createBook);
 
+// Acess : Private
 // PUT : update one book
 // Params query : id
 // Params body : title , authorId , datePublished , isFiction
@@ -25,6 +29,7 @@ router.put(
   BookController.updateBook
 );
 
+// Acess : Private
 // DELETE : delete a book
 // Params query : id
 router.delete('/:id', BookController.checkExistingBook, BookController.deleteBook);
