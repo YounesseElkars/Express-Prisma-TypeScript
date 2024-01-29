b2b63c9e978b10d1d34e2a92841f838740b96898
Unesdevdev <y.ounesseelkars9@gmail.com>
2024-01-29 23:58:33 +0100

    feat : error handler

--- src/middleware/error-handler.ts
+++ src/middleware/error-handler.ts
@@ -2,6 +2,7 @@ import { Prisma } from '@prisma/client';
 import { Request, Response, NextFunction } from 'express';
 import { z } from 'zod';
 import HttpStatusCode from '../utils/HttpStatusCode';
+import { JsonWebTokenError } from 'jsonwebtoken';
 
 export const errorHandler = (error: any, request: Request, response: Response, next: NextFunction) => {
   // Log the error stack for debugging purposes
@@ -22,8 +23,18 @@ export const errorHandler = (error: any, request: Request, response: Response, n
   // Handle known Prisma errors
   if (error instanceof Prisma.PrismaClientKnownRequestError) {
     const res =
-      process.env.NODE_ENV == 'developement'
+      process.env.APP_ENV == 'developement'
         ? { error: 'Prisma Error occurred', details: error }
+        : { error: 'Error occurred' };
+
+    return response.status(HttpStatusCode.INTERNAL_SERVER_ERROR).json(res);
+  }
+
+  // Handle Json Web Token Error
+  if (error instanceof JsonWebTokenError) {
+    const res =
+      process.env.APP_ENV == 'developement'
+        ? { error: 'Json Web Token Error occurred', message: error }
         : { error: 'Error occurred' };
 
     return response.status(HttpStatusCode.INTERNAL_SERVER_ERROR).json(res);
@@ -31,7 +42,7 @@ export const errorHandler = (error: any, request: Request, response: Response, n
 
   // Handle other types of errors
   const res =
-    process.env.NODE_ENV == 'developement'
+    process.env.APP_ENV == 'developement'
       ? { success: false, error: 'Internal Server Error', message: error.message }
       : { success: false, error: 'Internal Server Error' };
   return response.status(HttpStatusCode.INTERNAL_SERVER_ERROR).json(res);
