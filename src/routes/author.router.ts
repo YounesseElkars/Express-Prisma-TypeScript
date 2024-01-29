a1208ae688a16cbfa27d7b4394edb6ce10a4ac65
Unesdevdev <y.ounesseelkars9@gmail.com>
2024-01-29 23:58:52 +0100

    feat : athor router

--- src/routes/author.router.ts
+++ src/routes/author.router.ts
@@ -3,17 +3,21 @@ import * as AuthorController from '../controllers/author.controller';
 
 const router = express.Router();
 
+// Acess : Public
 // GET : Get List of all authors
 router.get('/', AuthorController.listAuthors);
 
+// Acess : Public
 // GET : Get One author by ID
 // Params query : id
 router.get('/:id', AuthorController.checkExistingAuthor, AuthorController.getAuthor);
 
+// Acess : Private
 // POST : Create one author
 // Params body : firstName , lastName
 router.post('/', AuthorController.validateAuthorData, AuthorController.createAuthor);
 
+// Acess : Private
 // PUT : update an author
 // Params query : id
 // Params body : firstName , lastName
@@ -24,6 +28,7 @@ router.put(
   AuthorController.updateAuthor
 );
 
+// Acess : Private
 // DELETE : delete an author
 // Params query : id
 router.delete('/:id', AuthorController.checkExistingAuthor, AuthorController.deleteAuthor);
